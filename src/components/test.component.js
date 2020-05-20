eb6d03662673d168c44568dca02f38b531552c34
JckD <c16335383@mydit.ie>
2020-05-21 00:59:42 +0100

    error tracking in progress

--- src/components/test.component.js
+++ src/components/test.component.js
@@ -23,7 +23,14 @@ export default class TypingTest extends Component {
             typed_chars: '',
             current_quote_word: '',
             bg_colour: 'white',
-            count: 0
+            count: 0,
+            error_count : 0,
+            input_disabled : false,
+            quote_left : '',
+            quote_right : '',
+            quote_error : '',
+            quote_start : '',
+
         }
     }
 
@@ -41,7 +48,8 @@ export default class TypingTest extends Component {
             quote_words : words,
             current_quote_word : words[state.count],
             char_array : chars,
-            current_quote_char : chars[state.count]
+            current_quote_char : chars[state.count],
+            quote_start : state.quote_body
         }))
         
     }
@@ -49,6 +57,16 @@ export default class TypingTest extends Component {
     // Called whenever the user input is changed
     // Sets the state of user_input to the user's input and calls compare with the current word from state.
     onInputChange (e) {
+
+        if (this.state.count === 0) {
+            //Start timer
+
+            this.setState ({
+                quote_start : ''
+            })
+        }
+
+
         this.setState({
             user_input : e.target.value
         }, () => this.compare(this.state.current_quote_char))
@@ -63,9 +81,13 @@ export default class TypingTest extends Component {
     compare (current_word) {
         
         if (current_word === this.state.user_input) {
-            console.log("match")
-            if (this.state.count === this.state.char_array.length ) {
-                this.endTest();
+            console.log("match");
+            console.log('count: ' + this.state.count);
+            console.log('charrarry length: ' + this.state.char_array.length)
+            if (this.state.count === this.state.char_array.length -1) {
+                this.setState((state) => ({
+                    typed_chars: state.typed_chars + state.user_input
+                }), () => this.endTest())
             }
             else {
                 this.setState((state) => ({
@@ -74,16 +96,29 @@ export default class TypingTest extends Component {
                     count: state.count + 1,
                     bg_colour : 'white',
                     typed_chars: state.typed_chars + state.user_input,
-                    user_input : ''
+                    quote_left : state.typed_chars + state.user_input,
+                    quote_right : state.char_array.slice(state.count+2, state.char_array.length),
+                    user_input : '',
+                    quote_error: '',
+
                 }));  
             }
                 document.getElementById('input').value = '';
         }
         else {
-            console.log('bad')
+            //let errcount = this.state.error_count/2
+            let errorPos = this.state.count;
             this.setState((state) => ({
-                bg_colour : '#ff6666'
-            }))
+                //bg_colour : '#ff6666',
+
+                quote_error : state.char_array[state.count+1],
+                current_quote_char : '',
+                count: state.count + 1,
+                typed_chars : state.typed_chars + state.char_array[state.count+1],
+                quote_left : state.typed_chars,
+                quote_right : state.char_array.slice(state.count +2, state.char_array.length),
+                error_count : state.error_count + 1
+            }), () => console.log('Error count:' + this.state.error_count))
         }
     }
 
@@ -94,13 +129,14 @@ export default class TypingTest extends Component {
             quote_name: 'Test Quote Title',
             quote_body : 'This is a test quote that I am hardcoding to make web app',
             user_input : '',
-            //quote_words: [],
             current_quote_char : '',
             current_quote_word: '',
             typed_chars : '',
+            error_count : 0,
+            bg_colour: 'white',
             count: 0,},
             () => {
-                        console.log(this.state)
+                console.log(this.state)
             })
             console.log(this.state)
     }
@@ -109,7 +145,15 @@ export default class TypingTest extends Component {
     // Calculates word per minute
     endTest () {
         console.log('Test is over!')
-        this.resetTest();
+        //this.resetTest();
+        console.log('End error count before dividing : ' + this.state.error_count)
+        let errcount = this.state.error_count/2 
+        this.setState((state) => ({
+            error_count : state.error_count/2,
+            input_disabled : true,
+            //current_quote_char : 'TEST FINISHED'
+        }), () => console.log('end error count: ' + this.state.error_count))
+
         
     }
 
@@ -117,25 +161,28 @@ export default class TypingTest extends Component {
         return (
             <div className="container">
                 <h4>{this.state.quote_name}</h4>
-                
-                <table style={{borborderWidth: 5,  borderColor: "white", borderStyle: "solid"}}>
-                    <tbody>
-                         <tr>
-                            <Highlighter 
-                                highlightClassName="HighlightClass"
-                                searchWords={[this.state.typed_chars]}
-                                autoEscape={true}
-                                textToHighlight={this.state.quote_body}
-                                highlightStyle={{backgroundColor : "lightgreen"}}
-                                activeIndex={0}
-                                caseSensitive={true}
-                            />
-                        </tr>
-                    </tbody>      
-                </table>
+
+                    
+                <div>
+                    <span className="quote-start">{this.state.quote_start}</span>
+                    <span className="quote-left">{this.state.quote_left}</span>
+                    <span className="quote-current">{this.state.current_quote_char}</span>
+                    <span className="quote-error">{this.state.quote_error}</span>
+                    <span className="quote-right">{this.state.quote_right}</span>
+                </div>   
+
+                {/* <Highlighter 
+                highlightClassName="HighlightClass"
+                searchWords={[this.state.typed_chars]}
+                autoEscape={true}
+                textToHighlight={this.state.quote_body}
+                highlightStyle={{backgroundColor : "lightgreen"}}
+                activeIndex={1}
+                caseSensitive={true}
+                /> */}
                 <h5>Current Character: {this.state.current_quote_char}</h5>
-                 <br></br>   
-                <input type="text" onChange={this.onInputChange} id='input' style={{backgroundColor : this.state.bg_colour}}></input>
+                <br></br>   
+                <input type="text" onChange={this.onInputChange} id='input' style={{backgroundColor : this.state.bg_colour}} disabled={this.state.input_disabled}></input>
                 <button onClick={this.resetTest} style={{marginLeft: 10}} className="btn btn-light">
                     <svg className="bi bi-arrow-repeat" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" d="M2.854 7.146a.5.5 0 00-.708 0l-2 2a.5.5 0 10.708.708L2.5 8.207l1.646 1.647a.5.5 0 00.708-.708l-2-2zm13-1a.5.5 0 00-.708 0L13.5 7.793l-1.646-1.647a.5.5 0 00-.708.708l2 2a.5.5 0 00.708 0l2-2a.5.5 0 000-.708z" clipRule="evenodd"/>
