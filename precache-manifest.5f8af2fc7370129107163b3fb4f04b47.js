f27b0069e49786ec2929de58bbc0d65c5740883f
JckD <c16335383@mydit.ie>
2020-06-29 16:37:59 +0100

    Updates

---
+++ precache-manifest.5f8af2fc7370129107163b3fb4f04b47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7f92d5c3c940f0cc83269f4cd2c350",
+    "url": "/Typing-Test-App/index.html"
+  },
+  {
+    "revision": "ebdef187c02758263f5f",
+    "url": "/Typing-Test-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6c8e25f383bcdf10d2ad",
+    "url": "/Typing-Test-App/static/css/main.d4030cfa.chunk.css"
+  },
+  {
+    "revision": "ebdef187c02758263f5f",
+    "url": "/Typing-Test-App/static/js/2.434f9190.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Typing-Test-App/static/js/2.434f9190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8e25f383bcdf10d2ad",
+    "url": "/Typing-Test-App/static/js/main.05063f1f.chunk.js"
+  },
+  {
+    "revision": "13306b845561af67f1c6",
+    "url": "/Typing-Test-App/static/js/runtime-main.a02d7dfb.js"
+  },
+  {
+    "revision": "352de28dfd3df232b940ddeb35be162f",
+    "url": "/Typing-Test-App/static/media/B&B.352de28d.png"
+  }
+]);
