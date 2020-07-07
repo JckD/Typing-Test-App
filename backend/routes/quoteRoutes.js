a2cc7f1ba4d3ec9ffab1dd76d34b4e8e8669bcd2
JckD <c16335383@mydit.ie>
2020-07-07 14:23:49 +0100

    added update/:id route and comments

--- backend/routes/quoteRoutes.js
+++ backend/routes/quoteRoutes.js
@@ -3,6 +3,7 @@ const QuoteRoutes = express.Router();
 
 let Quote = require('../schemas/quote.model');
 
+//Route that returns all quotes
 QuoteRoutes.route('/').get(function(req, res){
     Quote.find(function(err, quotes){
         if (err) {
@@ -14,6 +15,8 @@ QuoteRoutes.route('/').get(function(req, res){
     });
 });
 
+
+// Ruote that returns one book that matches the requested id
 QuoteRoutes.get('/:id', async (req, res) => {
     let id = req.params.id
     try {
@@ -24,6 +27,31 @@ QuoteRoutes.get('/:id', async (req, res) => {
     }
 })
 
+// Route that find's a quote by its id and updates it with the
+// contents of the body of the request
+QuoteRoutes.post('/update/:id', async (req, res) => {
+    Quote.findById(req.params.id, function(err, quote) {
+        if (!quote) {
+            res.status(404).send("No quotes matching that id");
+        }
+        else {
+            quote.quoteTitle = req.body.quoteTitle;
+            quote.quoteBody = req.body.quoteBody;
+            quote.quoteAuthor = req.body.quoteAuthor;
+            quote.quoteUser = req.body.quoteUser;
+
+            quote.save().then(quote => {
+                res.json("Quote Updated!");
+            })
+            .catch(err => {
+                res.status(400).send("Quote not updated :(");
+            });
+        }
+    });
+});
+
+// Route that adds a quote to the database with the body of the request
+// containing the data for the quote 
 QuoteRoutes.route('/add').post(function (req, res){
     let quote = new Quote(req.body);
 
