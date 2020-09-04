46271cd7f1430bd9e3cf8c3601f751cdc9df612f
JckD <c16335383@mydit.ie>
2020-09-04 19:34:36 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -48,13 +48,11 @@ const App = () => {
                   <Link to="/quotes" className="nav-link">Quotes</Link>
                   <Link to="/createQuote" className="nav-link">Create Quote</Link>
                   <NavDropdown title="Account">
-                    <NavDropdown.Item href="/account">   
-                      
+                    <NavDropdown.Item href="/account">             
                         Profile
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                           <path fillRule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                        </svg> 
-                      
+                        </svg>           
                     </NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="http://localhost:8080/logout">Logout
