00aaf72ceb569ab94ce53585f6110ddca8b623b3
JckD <c16335383@mydit.ie>
2021-05-13 21:25:25 +0100

    bruh

---
+++ build/precache-manifest.1876f33e7ad8d2a5f2bb68c7698b5a7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dc0ceadcbd3392df1c4fe1b35c7b2d",
+    "url": "/Typing-Test-App/index.html"
+  },
+  {
+    "revision": "2bcac458bef9cb130d58",
+    "url": "/Typing-Test-App/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "fa59e8a2e73bfc01d550",
+    "url": "/Typing-Test-App/static/css/main.b9f273a6.chunk.css"
+  },
+  {
+    "revision": "2bcac458bef9cb130d58",
+    "url": "/Typing-Test-App/static/js/2.34a1ef2c.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/Typing-Test-App/static/js/2.34a1ef2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa59e8a2e73bfc01d550",
+    "url": "/Typing-Test-App/static/js/main.37772175.chunk.js"
+  },
+  {
+    "revision": "19547b4f800031b8ebc5",
+    "url": "/Typing-Test-App/static/js/runtime-main.c39adce7.js"
+  }
+]);
