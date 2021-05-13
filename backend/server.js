33da03701ca8b99317a227e95f7e50d06c8917f1
JckD <c16335383@mydit.ie>
2021-05-13 21:31:56 +0100

    dot env file

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const PORT = 8080;
 const router = express.Router();
-require('dotenv').config({ path: 'C:/Users/Jack/Documents/Typing-Test-App/backend/.env'});
+require('dotenv').config({ path: './.env'});
 
 
 //mongo db URI
